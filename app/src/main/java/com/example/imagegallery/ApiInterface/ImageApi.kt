b7a085bccbc68b7e944d6909edd1361ba6da0f45
kanti_s_sonagra <kantisonagra7807@gmail.com>
2023-04-26 22:43:22 +0530

    fix: end point added in request call

--- app/src/main/java/com/example/imagegallery/ApiInterface/ImageApi.kt
+++ app/src/main/java/com/example/imagegallery/ApiInterface/ImageApi.kt
@@ -6,6 +6,6 @@ import retrofit2.http.GET
 
 interface ImageApi {
 
-    @GET()
-    fun fetchImage() : Response<FlickrResponse>
+    @GET("?method=flickr.photos.getRecent&per_page=20&page=1&api_key=9150f9f8653b0f6c1e81928665476c3a&format=json&nojsoncallback=1&extras=url_s")
+    suspend fun fetchImage() : Response<FlickrResponse>
 }
